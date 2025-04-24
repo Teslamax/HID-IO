5ed6b43a86761d56f87435538244ff5eb6afc9f5
Ryan Rasmussen <Teslamax@users.noreply.github.com>
2025-04-24 12:17:56 -0500

    Update usb_gamepad.cpp

--- firmware/HID-IO/src/usb_gamepad.cpp
+++ firmware/HID-IO/src/usb_gamepad.cpp
@@ -3,23 +3,24 @@
 #include "usb_gamepad.h"
 #include "usb_hid_report_desc.h"
 #include "tusb.h"
+#include <Arduino.h>
+
+#define GAMEPAD_BUTTON_PIN 7  // TODO: Set to actual pin for your hardware
 
 static bool gamepad_state = false;
 
+void usb_gamepad_setup() {
+  pinMode(GAMEPAD_BUTTON_PIN, INPUT_PULLUP);  // Initialize physical button input
+}
+
 void usb_gamepad_loop() {
   static bool prev_state = false;
+  bool current_state = digitalRead(GAMEPAD_BUTTON_PIN) == LOW;
 
-  // Simulated button input (toggle every call)
-  gamepad_state = !gamepad_state;
-
-  if (tud_hid_ready()) {
+  if (current_state != prev_state && tud_hid_ready()) {
     uint8_t report[2] = { 0 };
-    report[0] = gamepad_state ? 0x01 : 0x00;  // Button 1 pressed or released
+    report[0] = current_state ? 0x01 : 0x00;  // Button pressed or released
     tud_hid_report(REPORT_ID_GAMEPAD, report, sizeof(report));
   }
+  prev_state = current_state;
 }
-
-void usb_gamepad_setup() {
-  // TODO: Initialize button input and report as gamepad
-}
-
