6937b5e5715ddd9da5f1454ab820abe1ed85cf4d
Ryan Rasmussen <Teslamax@users.noreply.github.com>
2025-04-24 12:25:41 -0500

    Update usb_gamepad.cpp

--- firmware/HID-IO/src/usb_gamepad.cpp
+++ firmware/HID-IO/src/usb_gamepad.cpp
@@ -5,7 +5,7 @@
 #include "tusb.h"
 #include <Arduino.h>
 
-#define GAMEPAD_BUTTON_PIN 7  // TODO: Set to actual pin for your hardware
+#define GAMEPAD_BUTTON_PIN 18  // P0.18 = USER/RESET button on dongle
 
 static bool gamepad_state = false;
 
@@ -19,7 +19,7 @@ void usb_gamepad_loop() {
 
   if (current_state != prev_state && tud_hid_ready()) {
     uint8_t report[2] = { 0 };
-    report[0] = current_state ? 0x01 : 0x00;  // Button pressed or released
+    report[0] = current_state ? 0x01 : 0x00;  // Button 1 pressed or released
     tud_hid_report(REPORT_ID_GAMEPAD, report, sizeof(report));
   }
   prev_state = current_state;
